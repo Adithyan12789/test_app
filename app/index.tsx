08d0e28b53e66ffd021c2195ae4e7b9dfc739822
adithyanNarayanan <adithyan.narayanan@orbitcoders.com>
2025-11-04 12:01:17 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
+  );
+}
